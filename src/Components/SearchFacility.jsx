6b0bff7c1c41244bf46e85a412223e3d41184cde
Saikat Sarkar <sarkarsaikat3467@gmail.com>
2026-05-29 14:02:41 +0600

    Implemented Search Functionality and Filtering in All-Facilities route

---
+++ src/Components/SearchFacility.jsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { Label, SearchField, Button, Dropdown } from "@heroui/react";
+
+export default function SearchFacility() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const [search, setSearch] = useState(searchParams.get("search") || "");
+    const [sportType, setSportType] = useState(searchParams.get("sportType") || "");
+
+    const updateURL = (newSearch, newSportType) => {
+        const params = new URLSearchParams(searchParams.toString());
+
+        if (newSearch !== undefined) {
+            if (newSearch) params.set("search", newSearch);
+            else params.delete("search");
+        }
+
+        if (newSportType !== undefined) {
+            if (newSportType) params.set("sportType", newSportType);
+            else params.delete("sportType");
+        }
+        console.log(newSportType);
+        router.push(`?${params.toString()}`);
+    };
+
+    return (
+        <div className="flex gap-4 items-center">
+
+
+            <SearchField name="search">
+                <SearchField.Group>
+                    <SearchField.SearchIcon />
+
+                    <SearchField.Input
+                        className="w-[280px]"
+                        placeholder="Search Facility..."
+                        value={search}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setSearch(value);
+                            updateURL(value, sportType);
+                        }}
+                    />
+
+                    <SearchField.ClearButton
+                        onClick={() => {
+                            setSearch("");
+                            updateURL("", sportType);
+                        }}
+                    />
+                </SearchField.Group>
+            </SearchField>
+
+
+            <Dropdown>
+                <Button aria-label="Menu" variant="secondary">
+                    {sportType || "Select Sport"}
+                </Button>
+
+                <Dropdown.Popover>
+                    <Dropdown.Menu
+                        onAction={(key) => {
+                            const value = String(key);
+                            setSportType(value);
+                            updateURL(search, value);
+                        }}
+                    >
+                        <Dropdown.Item id="Football">
+                            <Label>Football</Label>
+                        </Dropdown.Item>
+
+                        <Dropdown.Item id="Cricket">
+                            <Label>Cricket</Label>
+                        </Dropdown.Item>
+
+                        <Dropdown.Item id="Basketball">
+                            <Label>Basketball</Label>
+                        </Dropdown.Item>
+
+                        <Dropdown.Item id="Tennis">
+                            <Label>Tennis</Label>
+                        </Dropdown.Item>
+
+                        <Dropdown.Item
+                            id=""
+                            variant="danger"
+                        >
+                            <Label>Clear Filter</Label>
+                        </Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown.Popover>
+            </Dropdown>
+
+        </div>
+    );
+}
--- src/app/all-facilities/page.jsx
+++ src/app/all-facilities/page.jsx
@@ -1,18 +1,35 @@
 import FacilityCard from "@/Components/FacilityCard";
+import SearchFacility from "@/Components/SearchFacility";
 
 
-const AllFacilitiesPage = async () => {
 
-    const res = await fetch('http://localhost:5000/facility');
+const AllFacilitiesPage = async ({ searchParams }) => {
+
+    const params = await searchParams
+
+    const search = params?.search || "";
+    const sportType = params?.sportType || "";
+
+    console.log("SEARCH PARAMS:", params);
+
+    const res = await fetch(`http://localhost:5000/facility?search=${search}&sportType=${sportType}`,
+        { cache: "no-store" });
+
     const data = await res.json();
 
     console.log(data);
 
     return (
-        
-        <div className="mt-[60px]">
-            <h1 className="text-center text-4xl font-bold">All Facilities</h1>
-            <div className="max-w-7xl mx-auto grid grid-cols-3 gap-10">
+
+        <div className="mt-[60px] max-w-7xl mx-auto ">
+            <div className="flex justify-between items-center ">
+                <h1 className=" text-4xl font-bold">All Facilities</h1>
+                <div>
+                    <SearchFacility></SearchFacility>
+                </div>
+            </div>
+
+            <div className=" grid grid-cols-3 gap-10 mt-[30px]">
                 {
                     data.map(d => <FacilityCard key={d._id} data={d}></FacilityCard>)
                 }
